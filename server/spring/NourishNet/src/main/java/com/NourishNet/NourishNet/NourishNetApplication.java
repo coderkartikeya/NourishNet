cd5f9891cf00d6fc1332feaac93823d92ff179a7
coderkartikeya <kartikeyavats04@gmail.com>
2026-01-18 15:47:08 +0530

    chore: initialize project scaffold for NourishNet
    Set up basic directory structure for Spring Boot (Trust Layer), Node.js (Speed Layer), and React (Frontend).

---
+++ server/spring/NourishNet/src/main/java/com/NourishNet/NourishNet/NourishNetApplication.java
@@ -0,0 +1,13 @@
+package com.NourishNet.NourishNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NourishNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NourishNetApplication.class, args);
+	}
+
+}
---
+++ server/spring/NourishNet/src/test/java/com/NourishNet/NourishNet/NourishNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NourishNet.NourishNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NourishNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
